e865f86795b556a943daabbfa13a7f6c1d1384fa
Hitesh <hiteshsaini4834383@gmail.com>
2026-05-06 12:47:35 +0530

    Complete Creator Hub Flutter App - Final Submission

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:get/get.dart';
-//
-// import 'modules/auth/login/login_view.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return ScreenUtilInit(
-//       designSize: const Size(375, 812), // standard UI size
-//       minTextAdapt: true,
-//       splitScreenMode: true,
-//       builder: (context, child) {
-//         return GetMaterialApp(
-//           debugShowCheckedModeBanner: false,
-//           title: 'Creator Hub',
-//           home: const LoginView(),
-//         );
-//       },
-//     );
-//   }
-// }
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
